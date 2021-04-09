d3052beb5f7f8b00813611bbec89c6fd9207fa05
pluszinstagramkovetok <82085954+pluszinstagramkovetok@users.noreply.github.com>
2021-04-09 13:38:20 +0200

    404

--- pages/yoursite/index.js
+++ pages/yoursite/index.js
@@ -2,16 +2,9 @@ function ajanlat1(){
     var ajanlat1 = document.getElementById("ajanlatszoveg1");
     var ajalnat2 = document.getElementById("ajanlat2");
 
-    if (ajanlat1.style.display == "none"){
-
         ajanlat1.style.display === "block";
         ajalnat2.style.display === "none";
-    }
-    else {
-        ajanlat1.style.display === "block";
-        ajalnat2.style.display === "none";
-    }
-
+ 
 
 
 
