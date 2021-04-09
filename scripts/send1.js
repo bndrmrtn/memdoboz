0d7e1a1f989360794024a95fd2e77933bcc73a2c
pluszinstagramkovetok <82085954+pluszinstagramkovetok@users.noreply.github.com>
2021-04-09 18:52:13 +0200

    bigupdate

---
+++ scripts/send1.js
@@ -0,0 +1,21 @@
+function ajanlat1() {
+    document.getElementById("ajanlat2").style.display = "none";
+    document.getElementById("form1").style.display = "block";
+
+}
+
+
+function ajanlat2() {
+    document.getElementById("ajanlat1").style.display = "none";
+    document.getElementById("form2").style.display = "block";
+
+}
+
+
+function vissza() {
+    document.getElementById("ajanlat1").style.display = "block";
+    document.getElementById("ajanlat2").style.display = "block";
+    document.getElementById("form1").style.display = "none";
+    document.getElementById("form2").style.display = "none";
+}
+
