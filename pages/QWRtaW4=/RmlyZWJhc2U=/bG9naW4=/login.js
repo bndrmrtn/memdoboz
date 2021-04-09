931a529133a9c20afdb815e4423cc54c3bf07a62
pluszinstagramkovetok <82085954+pluszinstagramkovetok@users.noreply.github.com>
2021-04-09 10:58:52 +0200

    updates

---
+++ pages/QWRtaW4=/RmlyZWJhc2U=/bG9naW4=/login.js
@@ -0,0 +1,16 @@
+function validate(){
+
+        var username=document.getElementById("id").value;
+        var password=document.getElementById("pass").value;
+
+
+        if(username=="admin"&& password=="user"){
+            alert("Sikeres bejelentkezés");
+            window.open("https://memdoboz.xyz/L3BhZ2VzL1FXUnRhVzQ9L1JtbHlaV0poYzJVPS9iRzluYVc0PS9sb2dpbi5odG1s/L3BhZ2VzL1FXUnRhVzQ9L1JtbHlaV0poYzJVPS9iRzluYVc0PS9sb2dpbi5odG1s/L3BhZ2VzL1FXUnRhVzQ9L1JtbHlaV0poYzJVPS9iRzluYVc0PS9sb2dpbi5odG1s/L3BhZ2VzL1FXUnRhVzQ9L1JtbHlaV0poYzJVPS9iRzluYVc0PS9sb2dpbi5odG1s/L3BhZ2VzL1FXUnRhVzQ9L1JtbHlaV0poYzJVPS9iRzluYVc0PS9sb2dpbi5odG1s/admin.html")
+            return false;
+        }
+        else{
+            alert("Sikertelen bejelentkezés")
+            return false;
+        }
+}
